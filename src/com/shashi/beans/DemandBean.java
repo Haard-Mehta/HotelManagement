638692c2220b95e497b11fdc5569a7fbe72a4fb4
Haard-Mehta <mehta1e@uwindsor.ca>
2024-07-30 02:15:16 -0400

    Modified Method

--- src/com/shashi/beans/DemandBean.java
+++ src/com/shashi/beans/DemandBean.java
@@ -40,7 +40,7 @@ public class DemandBean implements Serializable {
 		return demandQty;
 	}
 
-	public void setDemandQty(int demandQty) {
+	public void settingDemandQuantity(int demandQty) {
 		this.demandQty = demandQty;
 	}
 
